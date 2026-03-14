ffd098ac5017c961ef01ee277ab36970b2675b83
ale <alessvanni98@gmail.com>
2026-03-14 23:33:36 +0100

    improved selection ui

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -18,20 +18,27 @@ refresh(gameroot,obj)
 
 function addTilesEventListeners(container, total){
     let tiles = container.querySelectorAll(".tile")
-    tiles.forEach( tile => {
+    for (const tile of tiles){
+        if (tile.classList.contains("EMPTY")){continue}
+
         tile.addEventListener("mouseenter" , ()=>{
             const groupClass = tile.classList[1]
             let group = container.querySelectorAll("."+groupClass)
             for (const t of group){t.classList.add("BLINK")}
         })
+
         tile.addEventListener("mouseout" , ()=>{
             container.querySelectorAll(".BLINK")
             .forEach(t => t.classList.remove("BLINK"));
         })
-        tile.addEventListener ("click",() =>{
-            for (const t of total){t.classList.remove("OPAQUE1")}
 
-            if (selected) for (const t of total){t.classList.remove("SELECTED")}
+        tile.addEventListener ("click",() =>{
+            if (tile.classList.contains("SELECTED")){
+                resetSelected(selected,total)
+                return
+            }
+                
+            resetSelected(selected,total)
             selected = container.querySelectorAll(".tile.BLINK")
             if (selected) for (const t of selected){t.classList.add("SELECTED")}
             
@@ -47,8 +54,16 @@ function addTilesEventListeners(container, total){
                 if (!t.classList.contains("SELECTED")){t.classList.add("OPAQUE1")} 
             }
             
-        }) 
-    })
+        })
+    }
+}
+
+function resetSelected(selected, total){
+    for (const t of total){t.classList.remove("OPAQUE1")}
+    if (selected) for (const t of total){
+        t.classList.remove("SELECTED")
+        selected=null
+    }
 }
 
 function addHandlersToDraw(){
@@ -58,6 +73,9 @@ function addHandlersToDraw(){
     // now each tile in fds and center is hoverable and can select a valid move
     fds.forEach(fd  => addTilesEventListeners(fd, drawTiles))
     centers.forEach(center => addTilesEventListeners(center, drawTiles))
+
+    let fdc = document.querySelector("factory-display-container")
+    fdc.addDrawEventListeners(fdc,drawTiles)
 }
 
 
